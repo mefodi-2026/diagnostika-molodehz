489914ca482760966d0972eae820e842178f64fd
mefodi-2026 <vzoha68@gmail.com>
2026-07-23 22:28:50 +0500

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,9 @@
+window.FIREBASE_CONFIG = {
+  apiKey: "AIzaSyBpVdB5dcyJ48GmSfuBoOH01Dq0lkzgo",
+  authDomain: "molodeh-c523e.firebaseapp.com",
+  databaseURL: "https://molodeh-c523e-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "molodeh-c523e",
+  storageBucket: "molodeh-c523e.firebasestorage.app",
+  messagingSenderId: "480205704504",
+  appId: "1:480205704504:web:528d39b76a684c2f20c5cd"
+};
